5485a001b9af118684241a5fec625950da02e7fd
Leduc2910 <minhduc293103@gmail.com>
2024-03-18 21:06:11 +0700

    Duc sua model cua Dan 18/03

--- src/main/java/com/codegym/model/User.java
+++ src/main/java/com/codegym/model/User.java
@@ -1,14 +1,13 @@
 package com.codegym.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.util.Date;
 
 @Entity
 @Data
@@ -16,14 +15,21 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty(message = "Thiếu username")
+    @NotEmpty(message = "Vui lòng nhập tài khoản")
+    @Column(unique = true)
     private String username;
+    @NotEmpty(message = "Vui lòng nhập mật khẩu")
     @Size(min = 6, max = 32)
     private String password;
+    @NotEmpty(message = "Vui lòng xác nhận mật khẩu")
     private String confirmPassword;
-    @NotEmpty(message = "Thiếu email")
+    @NotEmpty(message = "Vui lòng nhập email")
     @Email(message = "Email không hợp lệ")
+    @Column(unique = true)
     private String email;
+    @NotEmpty(message = "Vui lòng nhập số điện thoại")
     private String phone;
-    private String birthday;
+    @NotEmpty(message = "Vui lòng nhập ngày sinh")
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date birthday;
 }
