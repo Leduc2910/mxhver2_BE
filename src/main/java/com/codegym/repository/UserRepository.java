4699cabd997802dc29aa297013a61bf727a281e2
doande68 <doandinhde2k3@gmail.com>
2024-03-18 21:22:32 +0700

    service,repository

---
+++ src/main/java/com/codegym/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.codegym.repository;
+
+import com.codegym.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByNameContaining(String name);
+}
---
+++ src/main/java/com/codegym/service/UserService.java
@@ -0,0 +1,14 @@
+package com.codegym.service;
+
+import com.codegym.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    void save(User user);
+    void delete(Long id);
+    List<User> findALl();
+    Optional<User> findById(Long id);
+    List<User> findByName(String name);
+}
