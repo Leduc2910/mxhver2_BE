9cb6ab83c169d59f4641e936422884563e903589
Leduc2910 <minhduc293103@gmail.com>
2024-03-18 20:18:52 +0700

    Duc tao du an phia back end 18/03

---
+++ src/main/java/com/codegym/Mxhver2Application.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mxhver2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mxhver2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/mxhver2/Mxhver2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.mxhver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mxhver2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
